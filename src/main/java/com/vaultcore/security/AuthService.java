791c58f66cc107b6f258bc601045022d08e5841c
Karthik Muthuirulappan <karthikm930620@gmail.com>
2026-02-25 12:08:51 +0530

    Security Refactor: Remove internal JWT system and finalize OAuth2/OIDC Resource Server integration (Week 1 compliant)

--- src/main/java/com/vaultcore/security/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.vaultcore.security;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthService {
-    public record AuthTokens(String accessToken, String refreshToken) {}
-}
